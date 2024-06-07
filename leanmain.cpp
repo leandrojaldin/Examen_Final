ac575d0b9add19763edaaab491ca01f40430726f
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-06-07 08:25:58 -0400

    se  añadió mensajes de instrucciones para guiar al usurio sobre como utilizar los comando del progama en el menu principal

--- leanmain.cpp
+++ leanmain.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
+void menuPrincipal(){
+    string opcion;
+    string comando;
+    string evento;
+    string fechas;
+
+    do{
+        cout<<"\t\t\t\t   Registro de eventos   \t\t\t\t \n\n";
+        cout<<"Para agregar un nuevo evento se usa el comando: Add\n\n";
+        cout<<"Para elminar evento se usa el comando: Del\n\n"
+        cout<<"Para elminar un evento de una fecha especifica se usa el comando: del\n\n";
+        cout<<"Para buscar evento con fecha en especifico se usa el comando: Find\n\n";
+        cout<<"Para imprimir todos los eventos se usa el comando: Print\n\n";
+        cout<<"Para salir se usa el comando: Salir\n\n";
+        
+
+}
+}
+
+
+
+
 int main(){
-    int n;
-    cout<<<"enviando pruebas de commits al githup"
+
 }
